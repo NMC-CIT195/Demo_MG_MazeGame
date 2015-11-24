8d53e8297e84855d5d9138b5987b429d11adc1ea
John Velis <john@johnvelis.com>
2015-11-24 08:46:13 -0500

    initial commit

---
+++ Demo_MG_PlatformMovement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Demo_MG_PlatformMovement
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new PlatformMovement())
+                game.Run();
+        }
+    }
+#endif
+}
