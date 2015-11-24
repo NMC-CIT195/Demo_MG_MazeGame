768fdfa6ca8cb6bd130ebf1f1b25bf87800b6619
John Velis <john@johnvelis.com>
2015-11-24 08:49:04 -0500

    commit848

---
+++ Demo_MG_PlatformMovement/PlatformMovement.cs
@@ -0,0 +1,130 @@
+﻿using System; // add to allow Windows message box
+using System.Runtime.InteropServices; // add to allow Windows message box
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using System.Collections.Generic;
+
+namespace Demo_MG_PlatformMovement
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class PlatformMovement : Game
+    {
+
+
+        // add code to allow Windows message boxes when running in a Windows environment
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern uint MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        // set the cell size in pixels
+        private const int CELL_WIDTH = 64;
+        private const int CELL_HEIGHT = 64;
+
+        // set the map size in cells
+        private const int MAP_CELL_ROW_COUNT = 8;
+        private const int MAP_CELL_COLUMN_COUNT = 10;
+
+        // set the window size
+        private const int WINDOW_WIDTH = MAP_CELL_COLUMN_COUNT * CELL_WIDTH;
+        private const int WINDOW_HEIGHT = MAP_CELL_ROW_COUNT * CELL_HEIGHT;
+
+        // wall objects
+        Wall wall01;
+        Wall wall02;
+        Wall wall03;
+        Wall wall04;
+
+        // declare a GraphicsDeviceManager object
+        GraphicsDeviceManager graphics;
+
+        // declare a SpriteBatch object
+        SpriteBatch spriteBatch;
+
+        public PlatformMovement()
+        {
+            graphics = new GraphicsDeviceManager(this);
+
+            // set the window size 
+            graphics.PreferredBackBufferWidth = MAP_CELL_COLUMN_COUNT * CELL_WIDTH;
+            graphics.PreferredBackBufferHeight = MAP_CELL_ROW_COUNT * CELL_HEIGHT;
+
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // add floors, walls, and ceilings
+            wall01 = new Wall(Content, "wall", new Vector2(0, WINDOW_HEIGHT - CELL_HEIGHT));
+            wall01.Active = true;
+            wall02 = new Wall(Content, "wall", new Vector2(WINDOW_WIDTH - CELL_WIDTH, WINDOW_HEIGHT - CELL_HEIGHT));
+            wall02.Active = true;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // wall and player sprites loaded when instantiated
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            wall01.Draw(spriteBatch);
+            wall02.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
